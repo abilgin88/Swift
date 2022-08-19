e1c889bd295652b6f7463c07df338bca29d690aa
Abdullah Bilgin <abilgin02@icloud.com>
2022-08-19 18:56:42 +0300

    finish Mini-exercises

--- 05-functions.playground/Contents.swift
+++ 05-functions.playground/Contents.swift
@@ -119,3 +119,42 @@ func getValue() -> String {
 let valueInt: Int = getValue()
 let valueString: String = getValue()
 
+//: Mini-exercises
+/* Write a function named printFullName that takes two strings called firstName and lastName. The function should print out the full name defined as firstName + " " + lastName. Use it to print out your own full name.*/
+
+func printFullName(firstName: String, lastName: String) -> String {
+    firstName + " " + lastName
+}
+
+printFullName(firstName: "Abdullah", lastName: "Bilgin")
+
+/* Change the declaration of printFullName to have no external name for either parameter.*/
+
+func printFullName(_ firstName: String, _ lastName: String) -> String {
+    firstName + " " + lastName
+}
+
+printFullName("Abdullah", "Bilgin")
+
+/* . Write a function named calculateFullName that returns the full name as a string. Use it to store your own full name in a constant.*/
+
+func calculateFullName(_ firstName:String, _ lastName:String)
+-> String {
+    firstName + " " + lastName
+}
+
+let myFullName = calculateFullName("Abdullah", "Bilgin")
+
+
+/* Change calculateFullName to return a tuple containing both the full name and the length of the name. You can find a string’s length by using the count property. Use this function to determine the length of your own full name.
+*/
+
+func calculateFullName2(_ firstName:String, _ lastName:String)
+-> (name: String, lenght: Int) {
+    let fullName = firstName + " " + lastName
+    
+    return (fullName, fullName.count)
+
+}
+
+let fullNameLength = calculateFullName2("Abdullah", "Bilgin").lenght
