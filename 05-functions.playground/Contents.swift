bbdb5ada05502d2ac222d244cfd78305ddddf004
Abdullah Bilgin <abilgin02@icloud.com>
2022-08-20 20:49:16 +0300

    Functions -> Challenges

--- 05-functions.playground/Contents.swift
+++ 05-functions.playground/Contents.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Darwin
 
 //: Function basics
 // function declaration
@@ -215,4 +216,146 @@ calculateAverage(of: 4, and: 5, and: 3)
 /* Xcode shows your documentation when code completion comes up,*/
 
 /* you can hold the option key and click on the function name, and Xcode shows your documentation in a handy popover*/
+
+//:Challenges
+//:Challenge 1: Looping with stride functions
+/* In Chapter 4, “Advanced Control Flow”, you wrote some for loops with countable ranges. Countable ranges are limited in that they must always be increasing by one. The Swift stride(from:to:by:) and stride(from:through:by:) functions let you loop much more flexibly.
+
+For example, if you wanted to loop from 10 to 20 by 4’s you can write:
+ 
+for index in stride(from: 10, to: 22, by: 4) {
+ print(index)
+}
+ 
+// prints 10, 14, 18
+ 
+for index in stride(from: 10, through: 22, by: 4) {
+ print(index)
+}
+ 
+// prints 10, 14, 18, and 22
+ 
+• What is the difference between the two stride function overloads?
+• Write a loop that goes from 10.0 to (and including) 9.0, decrementing by 0.1.
+*/
+
+// --> the difference between the to and through is like 10...30 and 10..<30
+
+for index in stride(from: 10.0, through: 9.0, by: -0.1) {
+    print(index)
+}
+
+//:Challenge 2: It’s prime time
+/*
+ When I’m acquainting myself with a programming language, one of the first things I do is write a function to determine whether or not a number is prime. That’s your second challenge.
+ 
+ */
+
+func isNumberDivisible(_ number:Int, by divisor: Int) -> Bool {
+    number % divisor == 0
+}
+
+func isPrime(_ number: Int) -> Bool {
+    if number <= 0 {
+        return false
+    }
+    for i in 2..<number {
+        if isNumberDivisible(number, by: i) {
+        return false
+        }
+    }
+    return true
+}
+
+isPrime(-3)
+isPrime(0)
+isPrime(6) // false
+isPrime(13) // true
+isPrime(8893) // true
+isPrime(9)
+
+
+// another solution
+
+func isNumberDivisible2(_ number: Int, by divisor: Int) -> Bool {
+    number % divisor == 0
+}
+
+func isPrime2(_ number: Int) -> Bool {
+    if number < 0 {
+        return false
+    }
+    
+    if number <= 3 {
+        return true
+    }
+    
+    let doubleNumber = Double(number)
+    let root = Int(doubleNumber.squareRoot())
+    for divisor in 2...root {
+        if isNumberDivisible2(number, by: divisor){
+            return false
+        }
+            
+    }
+    return true
+}
+
+isPrime(6)
+isPrime(13)
+isPrime(8893)
+
+//:Challenge 3: Recursive functions
+/*
+ 
+ In this challenge, you will see what happens when a function calls itself, a behavior called recursion. This may sound unusual, but it can be quite useful.
+ You’re going to write a function that computes a value from the Fibonacci sequence. Any value in the sequence is the sum of the previous two values. The sequence is defined such that the first two values equal 1. That is, fibonacci(1) = 1 and fibonacci(2) = 1.
+ Write your function using the following declaration:
+ func fibonacci(_ number: Int) -> Int
+ Then, verify you’ve written the function correctly by executing it with the following
+ numbers:
+ Hint 1: For values of number less than 0, you should return 0.
+ Hint 2: To start the sequence, hard-code a return value of 1 when number equals 1 or
+ 2.
+ Hint 3: For any other value, you’ll need to return the sum of calling fibonacci with
+ number - 1 and number - 2.
+ fibonacci(1)  // = 1
+ fibonacci(2)  // = 1
+ fibonacci(3)  // = 2
+ fibonacci(4)  // = 3
+ fibonacci(5)  // = 5
+ fibonacci(10) // = 55
+ 
+ */
+
+
+func fibonacci(_ number: Int) -> Int {
+    if number <= 0 {
+        return 0
+    }
+    if number == 1 || number == 2 {
+        return 1
+    }
+    
+    return fibonacci(number - 1) + fibonacci(number - 2)
+}
+
+//fibonacci(0)
+//fibonacci(1)
+//fibonacci(2)
+//fibonacci(3)
+//fibonacci(4)
+fibonacci(5)
+//fibonacci(10)
+
+func test(_ number2: Int) -> Int {
+    if number2 == 1 {
+        return 1
+    }
+    
+    return test(number2 - 1) + test(number2 - 1)
+}
+
+test(4)
+
 
