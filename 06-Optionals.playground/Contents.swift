8774c2d1fd38028617d614bd431cbdcbe2f420ba
Abdullah Bilgin <abilgin02@icloud.com>
2022-08-22 19:41:51 +0300

    Challenge for optional

--- 06-Optionals.playground/Contents.swift
+++ 06-Optionals.playground/Contents.swift
@@ -184,3 +184,116 @@ if let unwrapped = optionalInt1 {
 optionalInt1 = nil
 mustHaveResult1 = optionalInt1 ?? 0
 
+//:Challenges
+//:Challenge 1: You be the compiler
+/* Which of the following are valid statements? */
+
+var name7: String? = "Ray" // <-- valid
+//var age7: Int = nil  // <-- is not valid
+let distance7: Float = 26.7  // <-- valid
+var middleName7: String? = nil  // <-- valid
+//:Challenge 2: Divide and conquer
+/*
+ 
+ First, create a function that returns the number of times an integer can be divided by another integer without a remainder. The function should return nil if the division doesn’t produce a whole number. Name the function divideIfWhole.
+ Then, write code that tries to unwrap the optional result of the function. There should be two cases: upon success, print "Yep, it divides \(answer) times", and upon failure, print "Not divisible :[".
+ Finally, test your function:
+ 1. Divide 10 by 2. This should print "Yep, it divides 5 times."
+ 2. Divide 10 by 3. This should print "Not divisible :[."
+ Hint 1: Use the following as the start of the function signature:
+ func divideIfWhole(_ value: Int, by divisor: Int) You’ll need to add the return type, which will be an optional!
+ Hint 2: You can use the modulo operator (%) to determine if a value is divisible by another; recall that this operation returns the remainder from the division of two numbers. For example, 10 % 2 = 0 means that 10 is divisible by 2 with no remainder, whereas 10 % 3 = 1 means that 10 is divisible by 3 with a remainder of 1.
+ 
+ */
+
+
+
+
+func divideIfWhole(_ value: Int, by divisor: Int) -> Int? {
+    if value % divisor == 0 {
+        print("Yep, it divides \(value / divisor) times.")
+    return value / divisor
+    }
+    print( "Not divisible :[.")
+    return nil
+}
+
+divideIfWhole(10, by: 2)
+divideIfWhole(10, by: 3)
+
+// Book Solution;
+
+func divideIfWhole2(_ value: Int, by divisor: Int) -> Int? {
+    if value % divisor == 0 {
+        return value / divisor
+    } else {
+        return nil
+    }
+}
+
+if let answer = divideIfWhole2(10, by: 2) {
+    print("Yep, it divides \(answer) times")
+} else {
+    print("Not divisible :[")
+}
+
+if let answer = divideIfWhole2(10, by: 3 ) {
+    print("Yep, it divides \(answer) times")
+} else {
+    print("Not divisible :[")
+
+}
+
+//:Challenge 3: Refactor and reduce
+let answer1 = divideIfWhole2(10, by: 2) ?? 0
+print("It divides \(answer1) times.")
+
+let answer2 = divideIfWhole(10, by: 3) ?? 0
+print("It divides \(answer2) times.")
+
+//:Challenge 4: Nested optionals
+let number: Int??? = 10
+
+// 1. Fully force unwrap and print number.
+print(number!!!)
+
+
+// 2. Optionally bind and print number with if let.
+
+if let test = number {
+    if let test1 = test {
+        if let test2 = test1{
+            print(test2)
+        }
+        
+    }
+}
+
+
+// another solution
+
+if let number = number {
+  if let number = number {
+    if let number = number {
+      print(number)
+    }
+  }
+}
+
+// Better way of optionally binding without all the {}
+
+if let n1 = number,
+   let n2 = n1,
+   let n3 = n2 {
+    print(n3)
+}
+
+func printNumber(_ number: Int???) {
+    guard let n1 = number, let n2 = n1, let n3 = n2 else {
+        return
+    }
+    
+print(n3)
+}
+
+printNumber(number)
