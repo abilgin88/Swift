a016e5a2e901d8aec676992863dd16a675c9da51
Abdullah Bilgin <abilgin02@icloud.com>
2022-10-31 04:54:27 +0300

    Adding all

--- topics/Handling Error/Handling Error/ContentView.swift
+++ topics/Handling Error/Handling Error/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+ 
+    var variousGreetings = ["Bonjour", "Konnichiwa", "Jambo"]
     var body: some View {
+        
+        let greeting1 = variousGreetings[0]
+        let greeting2 = variousGreetings[1]
+        let greeting3 = variousGreetings[2]
+
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(greeting1)
+            Text(greeting2)
+            Text(greeting3)
         }
         .padding()
     }
