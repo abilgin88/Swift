44d70eedc0b941bec14befedc968a1949168589a
Abdullah Bilgin <abilgin02@icloud.com>
2022-11-03 20:48:12 +0300

    Access control studying

---
+++ topics/Intermadiate/AccessControl.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+struct Cat {
+    public func speak() {
+        print("Meeeeoooowww")
+    }
+    
+    internal let color = "Black"
+    
+    fileprivate var age : Int = 2
+    
+    private var type = "Ragdoll"
+    
+}
+
+let scrambles = Cat()
+
+// Could call this from other modules
+scrambles.speak()
+
+// Could call this from anywhere within the module
+print(scrambles.color)
+
+// Can call this only within this file
+print(scrambles.age)
+
+// This is invalid because type is inaccessible outside of the Cat structure
+//print(scrambles.type)
