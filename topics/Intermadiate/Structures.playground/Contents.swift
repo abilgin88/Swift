b5bdcaae8ef58cdd48e8848d1aa02712e5fbc3ff
Abdullah Bilgin <abilgin02@icloud.com>
2022-11-04 11:45:55 +0300

    Creating a Structure

--- topics/Intermadiate/Structures.playground/Contents.swift
+++ topics/Intermadiate/Structures.playground/Contents.swift
@@ -3,4 +3,18 @@ import UIKit
 //: Structures
 //:What is a Structure
 //:Creating a Structure
-
+struct Name {
+    //properties
+}
+//:
+struct Dog {
+    var age: Int
+    var isGood: Bool
+}
+// singular names "Dog" instead of "Dogs"
+// has two properties = type variables( var age: Int)
+//:
+struct Book {
+    var title: String
+    var pages: Int
+}
