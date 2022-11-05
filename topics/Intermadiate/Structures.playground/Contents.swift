06db525ff4b121ad1ec9daebfb87e2cae056a662
Abdullah Bilgin <abilgin02@icloud.com>
2022-11-05 08:25:05 +0300

    Creating an Instance

--- topics/Intermadiate/Structures.playground/Contents.swift
+++ topics/Intermadiate/Structures.playground/Contents.swift
@@ -18,6 +18,16 @@ struct Book {
     var title: String
     var pages: Int
 }
+//:
+struct Building {
+    var address: String
+    var floors: Int
+    
+    init(address: String, floors: Int, color: String) {
+        self.address = address
+        self.floors = floors
+    }
+}
 //: Default Property Values
 struct Dog2 {
     var age = 0
@@ -37,3 +47,36 @@ struct Car {
 var reliantRobin = Car(numberOfWheels: 3)
 print(reliantRobin.numberOfWheels) // Not default property value
 print(reliantRobin.topSpeed) // default property value
+//: Creating an Instance
+// 1- we modeled some god traits dog have
+struct Dog3 {
+    var age = 0
+    var isGood = true
+}
+
+/*
+ 2- each dog is unique in their own way to represent this programmatically,
+ we have to create different instance of dogs.
+ */
+var eloise = Dog3()
+//:
+struct Book3 {
+    var pages = 0
+    var title = ""
+}
+//instance of Book3
+var myFavBook = Book3()
+//:
+//:
+struct Person {
+    var name: String
+    var age: Int
+    
+    init (name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+}
+
+// instance of Person
+var morty = Person(name: "Morty", age: 14)
