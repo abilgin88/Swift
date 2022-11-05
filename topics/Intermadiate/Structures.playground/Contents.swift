a483fb88ff20fb8250036f26cb5a1588c16e3430
Abdullah Bilgin <abilgin02@icloud.com>
2022-11-05 08:09:15 +0300

    Default Property Values

--- topics/Intermadiate/Structures.playground/Contents.swift
+++ topics/Intermadiate/Structures.playground/Contents.swift
@@ -18,3 +18,22 @@ struct Book {
     var title: String
     var pages: Int
 }
+//: Default Property Values
+struct Dog2 {
+    var age = 0
+    var isGood = true
+}
+//:
+struct Book2 {
+    var title = ""
+    var pages = 0
+}
+//:
+struct Car {
+    var numberOfWheels = 4
+    var topSpeed = 80
+}
+
+var reliantRobin = Car(numberOfWheels: 3)
+print(reliantRobin.numberOfWheels) // Not default property value
+print(reliantRobin.topSpeed) // default property value
