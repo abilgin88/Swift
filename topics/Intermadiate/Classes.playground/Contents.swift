56d3b83975736118eb1316fc454fc15412798ec5
Abdullah Bilgin <abilgin02@icloud.com>
2022-11-07 17:51:55 +0300

    Inheritance

--- topics/Intermadiate/Classes.playground/Contents.swift
+++ topics/Intermadiate/Classes.playground/Contents.swift
@@ -90,3 +90,91 @@ print(laRatatouille.name)
 print(laRatatouille.type)
 print(laRatatouille.rating)
 print(laRatatouille.delivery)
+//:Inheritance
+// So suppose we have a BankAccount class that looks like:
+
+class BankAccount {
+    var balance = 0.0
+    
+    func deposit(amount: Double) {
+        balance += amount
+    }
+    
+    func withdraw(amount: Double) {
+        balance -= amount
+    }
+}
+
+// And we want a new SavingsAccount class that inherits from BankAccount:
+class SavingAccount: BankAccount {
+    var interest = 0.0
+    
+    func addInterest() {
+        let interest = balance * 0.005
+        self.deposit(amount: interest)
+    }
+}
+
+/*
+ Here, the new SavingsAccount class (subclass) automatically gains all of the characteristics of BankAccount class (superclass), such as its .balance property and its .deposit() and .withdraw() methods.
+ 
+ In addition, the SavingsAccount class defines the following:
+
+ .interest property that stores the interest rate
+ .addInterest() method that deposits the interest
+ And only SavingsAccount instances will have these.
+ */
+var myAccount = BankAccount()
+
+myAccount.balance = 20
+print(myAccount.balance)
+
+myAccount.deposit(amount: 20)
+print(myAccount.balance)
+
+myAccount.withdraw(amount: 10)
+print(myAccount.balance)
+
+var saveMyAccount = SavingAccount()
+
+saveMyAccount.interest = 10
+print(myAccount.balance)
+print(saveMyAccount.balance)
+
+saveMyAccount.addInterest() // balance is 0 here (balance * 0.005)
+print(myAccount.balance)
+print(saveMyAccount.balance)
+
+saveMyAccount.balance = 10
+print(myAccount.balance)
+print(saveMyAccount.balance)
+
+saveMyAccount.addInterest() // adding balance (10 * 0,005)
+print(myAccount.balance)
+print(saveMyAccount.balance)
+
+saveMyAccount.deposit(amount: 5)
+print(myAccount.balance)
+print(saveMyAccount.balance)
+
+saveMyAccount.withdraw(amount: 10)
+print(myAccount.balance)
+print(saveMyAccount.balance)
+//:
+class Order {
+  var items = [""]
+  var subtotal = 0.0
+  var tip = 0.0
+  var total = 0.0
+
+  func printReceipt() {
+    print("Items:     \(items)")
+    print("Subtotal:  $\(subtotal)")
+    print("Tip:       $\(tip)")
+    print("Total:     $\(total)")
+  }
+}
+
+class DeliveryOrder: Order {
+    var deliveryFee = 2.0
+}
