be2da092a282ad4c7a8bb3fd968ba1c8c787c3b4
Abdullah Bilgin <abilgin02@icloud.com>
2022-11-07 16:57:19 +0300

    The Init Method

--- topics/Intermadiate/Classes.playground/Contents.swift
+++ topics/Intermadiate/Classes.playground/Contents.swift
@@ -40,3 +40,53 @@ print(bobsBurgers.name)
 print(bobsBurgers.type)
 print(bobsBurgers.rating)
 print(bobsBurgers.delivery)
+//:The Init Method
+class Fruit {
+    var hasSeeds = true
+    var color: String
+    
+    init(color: String) {
+        self.color = color
+    }
+}
+let apple = Fruit(color: "red")
+print(apple.hasSeeds)
+print(apple.color)
+//:
+class Student2 {
+    var name = ""
+    var year = 0
+    var gpa = 0.0
+    var honors = false
+    
+    init(name: String, year: Int, gpa: Double, honors: Bool) {
+        self.name = name
+        self.year = year
+        self.gpa = gpa
+        self.honors = honors
+    }
+}
+
+var bart = Student2(name: "Bart Simpson", year: 4, gpa: 0.0, honors: false)
+//:
+class Restaurant2 {
+  var name = ""
+  var type = [""]
+  var rating = 0.0
+  var delivery = false
+
+  // The init() goes here 🍝
+  
+  init(name: String, type: [String], rating: Double, delivery: Bool) {
+    self.name = name
+    self.type = type
+    self.rating = rating
+    self.delivery = delivery
+  }
+}
+var laRatatouille = Restaurant2(name: "La Ratatouille", type: ["French"], rating: 4.7, delivery: false)
+
+print(laRatatouille.name)
+print(laRatatouille.type)
+print(laRatatouille.rating)
+print(laRatatouille.delivery)
